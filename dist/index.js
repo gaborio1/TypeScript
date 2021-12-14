55a854b831ee77377e1989ef0273b65cff1a88e3
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-12-14 23:42:44 +0000

    add type: object

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 // let id: number = 5;
 // console.log("id: ", id);
 // ==================== TYPES ===================== 
-// ===== NUMBER =====
+// ===== number, boolean, string =====
 // const add = (n1, n2) => { // DEFAULT TYPE: any
 //     return n1 + n2;
 // }
@@ -18,21 +18,51 @@
 //         return n1 + n2;
 //     }
 // }
-// ADD TYPE ASSIGNMENTS TO PARAMETERS
+// ADD EXPLICIT TYPE ASSIGNMENTS TO PARAMETERS
 const add = (n1, n2, showResult, phrase) => {
     const result = n1 + n2;
     if (showResult) {
-        console.log(phrase + n1 + n2);
-        console.log(phrase, result);
+        console.log(phrase + n1 + n2); // Result is: 53
+        console.log(phrase, result); // Result is:  8
     }
     else {
-        return n1 + n2;
+        return result;
     }
 };
-const number1 = 5;
+// NO ASSIGNMENT HERE AS TS HAS TYPE INFERENCE:
+const number1 = 5; // INFERRED TO "number"
+// let number1 = 5;
 const number2 = 3;
-const printResult = true;
-const resultPhrase = "Result is: ";
+const printResult = true; // INFERRED TO "boolean"
+const resultPhrase = "Result is: "; // INFERRED TO "string"
+// BUT:
+let number3; // SPECIFY TYPE WHEN INITIALISING
+// number3 = "7";          // Type 'string' is not assignable to type 'number' !!!
+// let number4;            // UNLESS WE WANG "any" TYPE
 // const result = add(number1, number2, printResult);
 // console.log(result);   // 8
 add(number1, number2, printResult, resultPhrase);
+// ===== OBJECT =====
+// const person = {
+//     name: "max",    // name: string:
+//     age: 30         // age: number:
+// }
+//  GENERIC OBJECT TYPE INFERRED BY TS WITH key - type PAIRS WITH SEMICOLONS:
+// const person: {
+//     name: string;
+//     age: number;
+// }
+// const person: object = {
+//     name: "max",
+//     age: 30
+// }
+// GENERIC OBJECT TYPE INFERRED BY TS WITH key - type PAIRS WITH SEMICOLONS:
+// const person: object
+// SPECIALISED OBJECT TYPE WITH INFO ABOUT ITS STRUCTURE:
+const person = {
+    name: "max",
+    age: 30
+};
+console.log(person.name);
+// ===== ARRAY =====
+// 1 16 00
--- src/index.ts
+++ src/index.ts
@@ -38,14 +38,63 @@ const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
     }
 }
 
-const number1 = 5;
+// NO ASSIGNMENT HERE AS TS HAS TYPE INFERENCE:
+const number1 = 5;  // INFERRED TO "number"
+// let number1 = 5;
 const number2 = 3;
-const printResult = true;
-const resultPhrase = "Result is: ";
+const printResult = true;   // INFERRED TO "boolean"
+const resultPhrase = "Result is: "; // INFERRED TO "string"
+
+
+// BUT:
+let number3: number;    // SPECIFY TYPE WHEN INITIALISING
+// number3 = "7";          // Type 'string' is not assignable to type 'number' !!!
+// let number4;            // UNLESS WE WANG "any" TYPE
 
 // const result = add(number1, number2, printResult);
 // console.log(result);   // 8
 
 add(number1, number2, printResult, resultPhrase);
 
-// VID: 1 03 32
+
+
+// ===== OBJECT =====
+
+// const person = {
+//     name: "max",    // name: string:
+//     age: 30         // age: number:
+// }
+
+//  GENERIC OBJECT TYPE INFERRED BY TS WITH key - type PAIRS WITH SEMICOLONS:
+
+// const person: {
+//     name: string;
+//     age: number;
+// }
+
+// const person: object = {
+//     name: "max",
+//     age: 30
+// }
+
+// GENERIC OBJECT TYPE INFERRED BY TS WITH key - type PAIRS WITH SEMICOLONS:
+
+// const person: object
+
+
+// SPECIALISED OBJECT TYPE WITH INFO ABOUT ITS STRUCTURE:
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "max",
+    age: 30
+}
+
+console.log(person.name)
+
+
+
+// ===== ARRAY =====
+
+// 1 16 00
